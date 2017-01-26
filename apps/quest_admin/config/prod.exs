6c008cfc59ea927c5eaf8e0f548bb54831963347
Hugo Duksis <duksis@gmail.com>
2017-01-26 23:04:51 +0100

    Remove cach static manifest line from config

--- apps/quest_admin/config/prod.exs
+++ apps/quest_admin/config/prod.exs
@@ -15,7 +15,6 @@ config :quest_admin, QuestAdmin.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [schema: "https", host: System.get_env("HOST_NAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
