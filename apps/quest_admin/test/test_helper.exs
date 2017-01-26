2e03ba5b89b81ddcebd4c2669ffcb28109fc21fe
Hugo Duksis <duksis@gmail.com>
2017-01-27 00:00:40 +0100

    Starts endpoints in test helpers for non standalone apps

--- apps/quest_admin/test/test_helper.exs
+++ apps/quest_admin/test/test_helper.exs
@@ -2,3 +2,7 @@ ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(QuestAdmin.Repo, :manual)
 
+import Supervisor.Spec
+opts = [strategy: :one_for_one, name: QuestAdmin.TestSupervisor]
+Supervisor.start_link([supervisor(QuestAdmin.Endpoint, [])], opts)
+
--- apps/quest_api/test/test_helper.exs
+++ apps/quest_api/test/test_helper.exs
@@ -2,3 +2,6 @@ ExUnit.start
 
 Ecto.Adapters.SQL.Sandbox.mode(QuestApi.Repo, :manual)
 
+import Supervisor.Spec
+opts = [strategy: :one_for_one, name: QuestApi.TestSupervisor]
+Supervisor.start_link([supervisor(QuestApi.Endpoint, [])], opts)
